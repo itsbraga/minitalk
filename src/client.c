35180f0905e535004d440c6057819215f61650a0
Anna BRAGA <annabrag@made-f0Br6s4.clusters.42paris.fr>
2023-12-06 18:26:07 +0100

    save work

--- src/client.c
+++ src/client.c
@@ -6,7 +6,7 @@
 /*   By: annabrag <annabrag@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/24 19:52:25 by annabrag          #+#    #+#             */
-/*   Updated: 2023/12/05 20:07:20 by annabrag         ###   ########.fr       */
+/*   Updated: 2023/12/06 18:25:19 by annabrag         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,27 +28,38 @@ void	send_bits(int pid, char c)
 	}
 }
 
+void	handler_sigusr(int signum)
+{
+	int	end_char;
+
+	end_char = 0;
+	if (signum == SIGUSR1)
+		end_char = send_bits();
+	if (signum == SIGUSR2)
+	{
+		
+	}
+}
+
 int	main(int argc, char **argv)
 {
 	int	pid;
 	int	i;
 
 	i = 0;
-	if (argc == 3)
+	if (argc != 3)
 	{
-		pid = getpid();
-		while (argv[2][i] != '\0')
-		{
-			send_bits(pid, argv[2][i]);
-			i++;
-		}
+		ft_printf(RED"Error: invalid arguments.\nRESET");
+		ft_printf(YELLOW
+		"Try this format: ./client <pid> <message>\nRESET");
+		return (1);
 	}
-	else
+	pid = getpid();
+	signal(SIGUSR1, handler_sigusr);
+	signal(SIGUSR2, handler_sigusr);
+	while (1)
 	{
-		ft_putstr_color_fd(RED, "Error: invalid arguments.\n", 2);
-		ft_putstr_color_fd(YELLOW,
-			"Try this format: ./client <pid> <message>\n", 2);
-		exit(EXIT_FAILURE);	
+		send_bits(pid, argv[2][i]);
+		i++;
 	}
-	return (0);
 }
